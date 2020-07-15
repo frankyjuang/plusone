2f50e66b2929c06c5ee8997ae0fb5270afdfe3a6
Yi-Lin Juang <frankyjuang@gmail.com>
2020-07-15 18:18:03 +0800

    Add facebook login

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [accessToken, setAccessToken] = useState<string>();
+
+  const initFB = () => {
+    // @ts-ignore
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: "201593587946994",
+        cookie: true,
+        status: true,
+        xfbml: true,
+        version: "v7.0",
+      });
+      FB.Event.subscribe("auth.statusChange", (res) => {
+        console.log("statuschange", res);
+        setAccessToken(res.authResponse?.accessToken);
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      // @ts-ignore
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      // @ts-ignore
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  };
+
+  useEffect(() => {
+    initFB();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="fb-login-button"
+        data-size="large"
+        data-button-type="login_with"
+        data-layout="rounded"
+        data-auto-logout-link="true"
+        data-use-continue-as="true"
+        data-width=""
+      ></div>
+      {accessToken}
     </div>
   );
 }
