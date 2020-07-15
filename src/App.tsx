ce18033c6dd213a0dade98df6588a164e0055ef6
Yi-Lin Juang <frankyjuang@gmail.com>
2020-07-16 03:54:35 +0800

    Get comments of live videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,49 @@
+// Two ways to get comments of live videos.
+// Client side polling: able to retrieve older comments.
+// https://developers.facebook.com/docs/graph-api/reference/live-video/comments/
+// Real-time server side pushing: might drop comments if connection is lost.
+// https://developers.facebook.com/docs/graph-api/server-sent-events/endpoints/live-comments/
+// The former is used here.
+// Requires two permissions from user: pages_read_engagement, pages_read_user_content
+// https://developers.facebook.com/docs/facebook-login/permissions/#reference-pages_read_engagement
+
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+interface User {
+  id: string;
+  name: string;
+}
+
+interface Page {
+  id: string;
+  name: string;
+}
+
+interface LiveVideo {
+  id: string;
+  title: string;
+  description: string;
+}
+
+interface Comment {
+  id: string;
+  message: string;
+  created_time: string;
+  from: User | undefined;
+}
+
+interface FBResponse {
+  data?: any;
+  error?: any;
+}
+
 function App() {
+  const [userId, setUserId] = useState<string>();
   const [accessToken, setAccessToken] = useState<string>();
+  const [pages, setPages] = useState<Page[]>([]);
+  const [liveVideos, setLiveVideos] = useState<LiveVideo[]>([]);
+  const [comments, setComments] = useState<Comment[]>([]);
 
   const initFB = () => {
     // @ts-ignore
@@ -14,9 +55,10 @@ function App() {
         xfbml: true,
         version: "v7.0",
       });
-      FB.Event.subscribe("auth.statusChange", (res) => {
+      FB.Event.subscribe("auth.authResponseChange", (res) => {
         console.log("statuschange", res);
-        setAccessToken(res.authResponse?.accessToken);
+        setUserId(res.authResponse?.userID);
+        setAccessToken(res.authResponse.accessToken);
       });
     };
 
@@ -35,9 +77,67 @@ function App() {
     })(document, "script", "facebook-jssdk");
   };
 
+  const getManagedPages = (userId: string) => {
+    // https://developers.facebook.com/docs/graph-api/reference/user/accounts/
+    return new Promise<Page[]>((resolve, reject) =>
+      FB.api<FBResponse>(`/${userId}/accounts`, (res) => {
+        console.log("pages", res);
+        res.error ? reject(res.error) : resolve(res.data);
+      })
+    );
+  };
+
+  const getPageLiveVideos = (pageId: string) => {
+    // https://developers.facebook.com/docs/graph-api/reference/page/live_videos/
+    return new Promise<LiveVideo[]>((resolve, reject) =>
+      FB.api<object, FBResponse>(
+        `/${pageId}/live_videos`,
+        {
+          broadcast_status: ["LIVE"],
+          fields: "id,title,description",
+        },
+        (res) => {
+          console.log("live videos", res);
+          res.error ? reject(res.error) : resolve(res.data);
+        }
+      )
+    );
+  };
+
+  const getLiveVideoComments = (videoId: string) => {
+    // https://developers.facebook.com/docs/graph-api/reference/live-video/comments/
+    return new Promise<Comment[]>((resolve, reject) =>
+      FB.api<object, FBResponse>(
+        `/${videoId}/comments`,
+        {
+          live_filter: "no_filter",
+          order: "reverse_chronological",
+          fields: "id,created_time,from,message",
+        },
+        (res) => {
+          console.log("live video comments", res);
+          res.error ? reject(res.error) : resolve(res.data);
+        }
+      )
+    );
+  };
+
   useEffect(() => {
     initFB();
   }, []);
+
+  useEffect(() => {
+    const getPages = async () => {
+      if (userId) {
+        const pages = await getManagedPages(userId);
+        setPages(pages);
+      } else {
+        setPages([]);
+      }
+    };
+
+    getPages();
+  }, [userId, accessToken]);
 
   return (
     <div className="App">
@@ -49,8 +149,115 @@ function App() {
         data-auto-logout-link="true"
         data-use-continue-as="true"
         data-width=""
+        // https://developers.facebook.com/docs/facebook-login/permissions/#reference-pages_read_engagement
+        data-scope="pages_read_engagement,pages_read_user_content"
       ></div>
-      {accessToken}
+      <button
+        onClick={() => {
+          FB.login(() => {}, {
+            scope: "pages_read_engagement,pages_read_user_content",
+            auth_type: "rerequest",
+          });
+        }}
+      >
+        Grant permission to more/less pages
+      </button>
+
+      <h3>Pages</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Page Id</th>
+            <th>Page Name</th>
+            <th>Live Videos</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pages.map((p) => (
+            <tr key={p.id}>
+              <td>{p.id}</td>
+              <td>{p.name}</td>
+              <td>
+                <button
+                  onClick={async () => {
+                    const videos = await getPageLiveVideos(p.id);
+                    setLiveVideos(videos);
+                  }}
+                >
+                  Fetch
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Live Videos</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Video Id</th>
+            <th>Video Title</th>
+            <th>Video Description</th>
+            <th>Comments</th>
+          </tr>
+        </thead>
+        <tbody>
+          {liveVideos.map((v) => (
+            <tr key={v.id}>
+              <td>{v.id}</td>
+              <td>{v.title}</td>
+              <td>{v.description}</td>
+              <td>
+                <button
+                  onClick={async () => {
+                    const comments = await getLiveVideoComments(v.id);
+                    setComments(comments);
+                  }}
+                >
+                  Fetch
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Comments</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Comment Id</th>
+            <th>Comment Message</th>
+            <th>Created Time</th>
+            <th>From</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {comments.map((c) => (
+            <tr key={c.id}>
+              <td>{c.id}</td>
+              <td>{c.message}</td>
+              <td>{c.created_time}</td>
+              <td>
+                {c.from?.name} ({c.from?.id})
+              </td>
+              <td>
+                {c.message.includes("+1") && (
+                  <button
+                    onClick={() => {
+                      alert(`+1 from ${c.from?.name || "unknown"}!`);
+                    }}
+                  >
+                    Create Order
+                  </button>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
